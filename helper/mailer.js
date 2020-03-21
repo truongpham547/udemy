c7682bba007925fe87b5b4a6b9365b2461f96675
phamlamtruongag@gmail.com <phamlamtruongag@gmail.com>
2020-03-21 12:02:12 +0700

    account mail

--- helper/mailer.js
+++ helper/mailer.js
@@ -1,36 +1,34 @@
+const nodemailer = require("nodemailer");
 
-const nodemailer = require('nodemailer');
-
-
-function sendMail(targetEmail,subject,content){
-    return new Promise((resolve,reject)=>{
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'vungoctuan369@gmail.com',
-                pass: '01685904514'
-            }
-        });
-            
-        var mailOptions = {
-            from: 'Online Course Team',
-            to: targetEmail,
-            subject: subject,
-            text: content
-        };
-            
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-                resolve(true);
-            }
-        });
+function sendMail(targetEmail, subject, content) {
+  return new Promise((resolve, reject) => {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "devemail1357@gmail.com",
+        pass: "dev13572468"
+      }
     });
+
+    var mailOptions = {
+      from: "Online Course Team",
+      to: targetEmail,
+      subject: subject,
+      text: content
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Email sent: " + info.response);
+        resolve(true);
+      }
+    });
+  });
 }
 
-module.exports={
-    sendMail:sendMail
-}
+module.exports = {
+  sendMail: sendMail
+};
